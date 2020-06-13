91d3db5857ff02f92b98e401ddf97300e087b87d
dinoashmiRV <66861387+dinoashmiRV@users.noreply.github.com>
2020-06-13 13:37:18 +0530

    New file no : 1
    It deals with swapping numbers without using temporary variable

---
+++ Swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int var1,var2, temp;
+printf("enter two integersn");
+scanf("%d%d", &var1,&var2);
+printf("before swappingnfirst variable=%dn, second variable=%dn,var1,var2);
+Var1= var1+var2;
+Var2= var1-var2;
+Var1= var1-var2;
+print("after swappingfirst variable=%dn, second variable=%dn,var1,var2);
+return0;
+}
